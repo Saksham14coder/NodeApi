7211c32676b282623efd5a8489a400c1d61ea619
Saksham Tandel <112418122+Saksham14coder@users.noreply.github.com>
2024-10-29 16:26:58 +0530

    Update mongodb_client.ts

--- src/config/mongodb_client.ts
+++ src/config/mongodb_client.ts
@@ -5,7 +5,7 @@ let mongoDb : Db
 
 
 export async function connectToDatabase(){
-    const url = 'mongodb+srv://thorstark140:mongodb@12345@@cluster0.yjppncs.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0'
+    const url = 'mongodb+srv://thorstark140:mongodb%4012345@cluster0.yjppncs.mongodb.net/'
     const client = new MongoClient(url);
 
     mongoDb = client.db("notedb")
